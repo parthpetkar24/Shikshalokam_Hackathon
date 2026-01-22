faf046707d18bff1899062dc8e7b34d431502b22
parthpetkar24 <parth.petkar2020@gmail.com>
2026-01-22 21:06:00 +0530

    Final Changes

--- Front-End/dashboard/script.js
+++
@@ -1,6 +0,0 @@
-function goToDashboard() {
-    // For demo purposes, let's go to the Teacher Dashboard
-    window.location.href = "teacher.html"; 
-    window.location.href = "diet.html"; 
-    // Or create a logic to check user role
-}
